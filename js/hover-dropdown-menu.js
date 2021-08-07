0b42325deb1e905c665d8a7c3bae40dcab163880
Tran Thi Thao Nguyen <nguyen.tran@MBP02902.local>
2021-08-07 17:56:14 +0700

    home

---
+++ js/hover-dropdown-menu.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+});
